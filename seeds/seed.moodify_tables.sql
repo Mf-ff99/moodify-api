e88db81bff980bf10c69231d6abc657408ef54b3
Mark Force <force.j.mark@gmail.com>
2020-10-06 12:18:13 -0400

    implemented timestamp sorting and chart

--- seeds/seed.moodify_tables.sql
+++ seeds/seed.moodify_tables.sql
@@ -8,8 +8,9 @@ INSERT INTO moodify_users (user_name, password)
 VALUES
   ('dunder', '$2a$10$pA7DYxURtAGDDdsp/vvZtOxbtB/4vpq3u/Pt5TCQSv9vTbtD4FreO');
 
-INSERT INTO moodify_moods (current_mood, note, user_id)
+INSERT INTO moodify_moods (current_mood, note, user_id, date_added)
 VALUES
-    (6, 'today sucked ass', 1);
+    (6, 'today sucked ass', 1, '2020-09-04 11:37:30'),
+    (8, 'today sucked ass', 1, '2020-09-29 11:37:30');
 
 COMMIT;
